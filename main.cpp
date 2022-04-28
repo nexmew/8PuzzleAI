e202d82c66ba1ce31f175bee209a707ad818e5be
scottuzamaki <scottuzamaki@gmail.com>
2022-04-28 11:04:43 -0700

    accepts input vector, prints the menu and puzzle

--- main.cpp
+++ main.cpp
@@ -1,12 +1,45 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
+void printStartingMenu() {
+    cout << "-------------------------------------------------" << endl;
+    cout << "|                                               |" << endl;
+    cout << "|       Welcome to my 8 puzzle solver!          |" << endl;
+    cout << "|                                               |" << endl;
+    cout << "|    Please enter your puzzle as a string:      |" << endl;
+    cout << "|                                               |" << endl;
+    cout << "|                                               |" << endl;
+    cout << "-------------------------------------------------" << endl;
+}
+
+
+void printPuzzle(vector<vector<int>> input) {
+    for ( int i = 0; i < 3; i++ ) {
+        for ( int j = 0; j < 3; j++ ) {
+           cout << input[i][j] << endl;
+        }
+    }
+}
+
 int main() {
 
+    // Initializes the vector we read from the user to solve! 
+    vector<vector<int>> vec(3, vector<int> (3));
 
 
+    printStartingMenu();
+    cout << "Enter 8 puzzle: " << endl;
 
+
+    for ( int i = 0; i < 3; i++ ) {
+        for ( int j = 0; j < 3; j++ ) {
+            cin >> vec[i][j];
+        }
+    }
+
+    printPuzzle(vec);
 
 
 
