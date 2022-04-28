ef72b2191650bf4969c222cf6f242fccd1711f05
scottuzamaki <scottuzamaki@gmail.com>
2022-04-28 11:13:41 -0700

    updated menu interface

--- main.cpp
+++ main.cpp
@@ -3,7 +3,8 @@
 
 using namespace std;
 
-void printStartingMenu() {
+void printStartingMenu()
+{
     cout << "-------------------------------------------------" << endl;
     cout << "|                                               |" << endl;
     cout << "|       Welcome to my 8 puzzle solver!          |" << endl;
@@ -14,34 +15,38 @@ void printStartingMenu() {
     cout << "-------------------------------------------------" << endl;
 }
 
+void printPuzzle(vector<vector<int>> input)
+{
 
-void printPuzzle(vector<vector<int>> input) {
-    for ( int i = 0; i < 3; i++ ) {
-        for ( int j = 0; j < 3; j++ ) {
-           cout << input[i][j] << endl;
-        }
-    }
+    cout << "------------------" << endl;
+    cout << "|                |" << endl;
+    cout << "| " << input[0][0] << "     " << input[0][1] << "     " << input[0][2] << "  |" << endl;
+    cout << "|                |" << endl;
+    cout << "| " << input[1][0] << "     " << input[1][1] << "     " << input[1][2] << "  |" << endl;
+    cout << "|                |" << endl;
+    cout << "| " << input[2][0] << "     " << input[2][1] << "     " << input[2][2] << "  |" << endl;
+    cout << "|                |" << endl;
+    cout << "------------------" << endl;
 }
 
-int main() {
+int main()
+{
 
-    // Initializes the vector we read from the user to solve! 
-    vector<vector<int>> vec(3, vector<int> (3));
-
+    // Initializes the vector we read from the user to solve!
+    vector<vector<int>> vec(3, vector<int>(3));
 
     printStartingMenu();
     cout << "Enter 8 puzzle: " << endl;
 
-
-    for ( int i = 0; i < 3; i++ ) {
-        for ( int j = 0; j < 3; j++ ) {
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < 3; j++)
+        {
             cin >> vec[i][j];
         }
     }
 
     printPuzzle(vec);
-
-
 
     return 0;
 }
