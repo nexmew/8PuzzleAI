1c053d5345c38f5eba574309bf89e3799f1a1e59
scottuzamaki <scottuzamaki@gmail.com>
2022-04-29 18:38:34 -0700

    added local variables to move functions

--- state.cpp
+++ state.cpp
@@ -10,16 +10,28 @@ state::~state() {
 
 state* state::moveBlankUp() {
 
+    state* nextState;
+    int newPuzzle[9];
+
 }
 
 state* state::moveBlankDown() {
+
+    state* nextState;
+    int newPuzzle[9];
 
 }
 
 state* state::moveBlankLeft() {
 
+    state* nextState;
+    int newPuzzle[9];
+
 }
 
 state* state::moveBlankRight() {
+
+    state* nextState;
+    int newPuzzle[9];
 
 }
